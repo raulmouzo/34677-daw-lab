388a9e4c428b5acd5c3952d6e614422cb448a553
Raúl Mouzo <raul.mouzo@udc.es>
2025-02-28 10:30:43 +0100

    feat: add admin pages

---
+++ admin/js/base.js
@@ -0,0 +1,40 @@
+class HeaderComponent extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    async connectedCallback() {
+        const response = await fetch('header.html');
+        const content = await response.text();
+        this.innerHTML = content;
+    }
+}
+window.customElements.define('my-header', HeaderComponent);
+
+const primaryHeader = document.querySelector('my-header');
+const scrollWatcher = document.createElement('div');
+
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+    console.log(entries)
+    primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting);
+
+});
+
+navObserver.observe(scrollWatcher);
+
+
+class FooterComponent extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    async connectedCallback() {
+        const response = await fetch('footer.html');
+        const content = await response.text();
+        this.innerHTML = content;
+    }
+}
+window.customElements.define('my-footer', FooterComponent);
